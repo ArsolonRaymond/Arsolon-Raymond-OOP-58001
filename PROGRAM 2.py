cffe5223ba8f5799dee7254b7d8ca03f81a3a21a
Mon Arsolon <98925684+ArsolonRaymond@users.noreply.github.com>
2022-04-11 06:29:11 +0300

    Add files via upload

---
+++ PROGRAM 2.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import ttk
+window = Tk()
+window.geometry("500x250+10+20")
+window.title("Midterm in OOP")
+
+
+label = Label(window, text = "Enter your fullname:", fg = "red")
+label.place(x=60, y=70)
+
+txtfld = Entry(window, bd = 5)
+txtfld.place(x=265, y=65)
+
+button = Button(window, text = "Click to display your Fullname", fg = "red")
+button.place(x=50,y=105)
+
+txtfld = Entry(window, bd = 5)
+txtfld.place(x=265, y=105)
+
+
+window.mainloop()
